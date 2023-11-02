e5c01b4490737cb5dafce303163c37a5a705426b
softwaretechcode <softwaretechcode@gmail.com>
2023-11-02 17:30:08 +0530

    new UI And Updates

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import * as router from 'BACKEND/upload';
 import { BlogPostModel } from 'src/app/models/blogpost.model';
 import { BlogService } from 'src/app/services/blogposts.service';
 import { environment } from 'src/environments/environment.prod';
@@ -10,20 +11,31 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-  
+  load:boolean=true;
   slug: string;
   post: BlogPostModel;
   apiUrl=environment.apiUrl;
-  constructor(private activatedRoute: ActivatedRoute,private blog_service:BlogService) {
+  constructor(private activatedRoute: ActivatedRoute,private blog_service:BlogService,private routerLink:Router) {
     this.activatedRoute.paramMap.subscribe((x: { get: (arg0: string) => any; }) => {
       this.slug = x.get('slug');
+      this.load=true;
     });
   }
   ngOnInit(): void {
    this.blog_service.getSinglePost(this.slug).subscribe(
-    (post)=>{
-      this.post=post;
-      // console.log(this.post);
+    (responce)=>{
+      if(responce['statusCode']==200){
+        this.load=false;
+        this.post=responce['body'];
+      }
+      else if(responce['statusCode']==404 && responce['statusCode']==500){
+        this.routerLink.navigate['/404'];
+        
+      }
+    
+    },err=>{
+      
+      console.log(err);
       
     });
    
---
+++ src/app/myckeditor5/myckeditor-full-featured/myckeditor-full-featured.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myckeditor-full-featured',
+  templateUrl: './myckeditor-full-featured.component.html',
+  styleUrls: ['./myckeditor-full-featured.component.css']
+})
+export class MyckeditorFullFeaturedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
