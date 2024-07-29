bdd6d2a94510aaaba8824588c8629996506ffb88
kuulie-dev <152971234+kuulie-dev@users.noreply.github.com>
2024-07-30 02:10:33 +0530

    angular-14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { provideHttpClient, withInterceptorsFromDi, withFetch } from "@angular/common/http";
+import { NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent, provideNgDocApp, provideSearchEngine, NgDocDefaultSearchEngine, providePageSkeleton, NG_DOC_DEFAULT_PAGE_SKELETON, provideMainPageProcessor, NG_DOC_DEFAULT_PAGE_PROCESSORS } from "@ng-doc/app";
+import { NG_DOC_ROUTING, provideNgDocContext } from "@ng-doc/generated";
+import { RouterModule } from "@angular/router";
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -83,10 +87,13 @@ import { DatePipePipe } from './pipes/date-pipe.pipe';
     MatButtonModule,
     MatIconModule,
     AppRoutingModule,
-      
+      RouterModule.forRoot(NG_DOC_ROUTING, {scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', scrollOffset: [0, 70]}),
+      NgDocRootComponent,
+      NgDocNavbarComponent,
+      NgDocSidebarComponent
 ],
  
-  providers: [],
+  providers: [provideHttpClient(withInterceptorsFromDi(), withFetch()), provideNgDocContext(), provideNgDocApp(), provideSearchEngine(NgDocDefaultSearchEngine), providePageSkeleton(NG_DOC_DEFAULT_PAGE_SKELETON), provideMainPageProcessor(NG_DOC_DEFAULT_PAGE_PROCESSORS)],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
